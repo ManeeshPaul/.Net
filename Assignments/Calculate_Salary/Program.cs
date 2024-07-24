a4135609a806ca132de124a0c793136d03a5affb
maneesh <maneeshpaulvtl@gmail.com>
2024-07-24 11:40:48 +0530

    "Salary Calculation project added"

---
+++ Assignments/Calculate_Salary/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculate_Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int basicSalary;
+            double grossSalary, HRA, DA;
+            Console.WriteLine("Salary calculation");
+            Console.WriteLine("Enter your Basic salary");
+            basicSalary = Convert.ToInt32(Console.ReadLine());
+
+            if (basicSalary <= 10000)
+            {
+                HRA = basicSalary * 0.2;
+                DA = basicSalary * 0.8;
+            }
+            else if (basicSalary <= 20000)
+            {
+                HRA = basicSalary * 0.25;
+                DA = basicSalary * 0.9;
+            }
+            else
+            {
+                HRA = basicSalary * 0.3;
+                DA = basicSalary * 0.95;
+            }
+            grossSalary = basicSalary + HRA + DA;
+
+            Console.WriteLine("Gross Salary = " + grossSalary);
+        }
+    }
+}
