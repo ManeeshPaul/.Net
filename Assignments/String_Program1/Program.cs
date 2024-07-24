bff1e7d60c40f3c9b3f0504c18ee95ebd6a0ad82
maneesh <maneeshpaulvtl@gmail.com>
2024-07-24 11:47:15 +0530

    Added String Program for adding first character of the string at the front and back.

---
+++ Assignments/String_Program1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace String_Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStringValue;
+            char firstLetter;
+            Console.WriteLine("Add the First character of the string at the begining and end \n");
+            Console.Write("Enter the string : ");
+            inputStringValue = Console.ReadLine();
+
+            firstLetter = inputStringValue[0];
+
+            inputStringValue = firstLetter + inputStringValue + firstLetter;
+
+            Console.WriteLine("Final Result = " + inputStringValue);
+        }
+    }
+}
