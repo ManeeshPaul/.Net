1bdc52313b6be251e1d6d68e86100f4fcf353f9c
maneesh <maneeshpaulvtl@gmail.com>
2024-07-24 11:59:34 +0530

    Added string program 3 for add the first three charcters of the given string at the front and back of the string

---
+++ Assignments/String_Program3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace String_Program3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStringValue, finalResult, firstThreeLetters;
+            int length;
+
+            Console.WriteLine("Add the First three letters at the front and back of the string\n");
+            Console.Write("Enter the string : ");
+            inputStringValue = Console.ReadLine();
+
+            length = inputStringValue.Length;
+            if (length <= 3)
+            {
+                finalResult = inputStringValue + inputStringValue + inputStringValue;
+                Console.WriteLine("Final Result = " + finalResult);
+            }
+            else
+            {
+                firstThreeLetters = inputStringValue.Substring(0, 3);
+
+                finalResult = firstThreeLetters + inputStringValue + firstThreeLetters;
+
+                Console.WriteLine("Final Result = " + finalResult);
+            }
+        }
+    }
+}
