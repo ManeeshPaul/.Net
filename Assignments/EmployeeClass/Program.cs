380f681cbf4a3b7a9403746118b332d68ea6aea8
maneesh <maneeshpaulvtl@gmail.com>
2024-07-26 17:00:35 +0530

    "Added Employee class creation project with getdata and printdata

---
+++ Assignments/EmployeeClass/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EmployeeClass
+{
+    class Employee
+    {
+        public int EmployeeId;
+        public string EmployeeName;
+        public string Salary;
+        public string Gender;
+
+        public void GetData()
+        {
+            Console.Write("Enter EmployeeId : ");
+            EmployeeId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Employee Name : ");
+            EmployeeName = Console.ReadLine();
+            Console.Write("Enter Employee salary : ");
+            Salary = Console.ReadLine();
+            Console.Write("Enter Employee gender : ");
+            Gender = Console.ReadLine();
+        }
+        public void PrintData(Employee employee)
+        {
+            Console.WriteLine("\nEmployee Id : " + employee.EmployeeId);
+            Console.WriteLine("Employee Name : " + employee.EmployeeName);
+            Console.WriteLine("Employee Salary : " + employee.Salary);
+            Console.WriteLine("Employee Gender : " + employee.Gender);
+        }
+
+    }
+    internal class ClassesAndObejects
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.GetData();
+            emp1.PrintData(emp1);
+        }
+    }
+}
