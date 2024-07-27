88fd082fc5c2372e8305527cca981ac881fd5802
maneesh <maneeshpaulvtl@gmail.com>
2024-07-27 18:37:22 +0530

    Added multilevel inheritance demo program

---
+++ Assignments/MultilevelInheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MultilevelInheritance
+{
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    class C : B
+    {
+        public C()
+        {
+            Console.WriteLine("c");
+        }
+    }
+    internal class MutiLevelInheritance
+    {
+        static void Main(string[] args)
+        {
+            C c = new C();
+        }
+    }
+}
