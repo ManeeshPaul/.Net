41c68bce682eaae15a1d40a04e4216cf03e43ff6
maneesh <maneeshpaulvtl@gmail.com>
2024-07-27 18:42:45 +0530

    Added interface problem of Vehicle

---
+++ Assignments/InterfaceProblem/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace InterfaceProblem
+{
+    interface IVehiculo
+    {
+        public void Drive();
+        public bool Refuel(int amount);
+
+    }
+    class Car : IVehiculo
+    {
+        public int gasolineAmount;
+
+        public Car(int startingGasolineAmount)
+        {
+            gasolineAmount = startingGasolineAmount;
+        }
+
+        public void Drive()
+        {
+            if (gasolineAmount > 0)
+            {
+                Console.WriteLine("Driving");
+            }
+        }
+        public bool Refuel(int amount)
+        {
+
+            gasolineAmount += amount;
+            return true;
+
+        }
+    }
+
+    internal class InterfaceProblem
+    {
+        static void Main(string[] arg)
+        {
+            int gasolineRefuelAmount;
+            Car car = new Car(0);
+            Console.Write("Enter the amount of gasoline to refuel : ");
+            gasolineRefuelAmount = Convert.ToInt32(Console.ReadLine());
+
+            car.Refuel(gasolineRefuelAmount);
+            car.Drive();
+
+        }
+    }
+}
