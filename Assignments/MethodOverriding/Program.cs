6de5d96d3302bfa6f24f897b63874cbf152a5699
maneesh <maneeshpaulvtl@gmail.com>
2024-07-29 23:52:59 +0530

    MethodOverriding program added

---
+++ Assignments/MethodOverriding/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MethodOverriding
+{
+    class BaseClass
+    {
+        public BaseClass()
+        {
+            Console.WriteLine("Constructor of Base class");
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine("Base class");
+        }
+    }
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass() : base()   
+        {
+            Console.WriteLine("Constructor of Derived class");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("Derived Class");
+        }
+    }
+
+    internal class Overriding
+    {
+        static void Main(string[] args)
+        {
+            BaseClass b1 = new BaseClass();
+            b1.Show();
+
+            b1 = new DerivedClass();
+            b1.Show();
+        }
+    }
+}
