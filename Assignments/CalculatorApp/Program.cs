7bab87d3ca4d879ef5289649073945f8e5d02173
maneesh <maneeshpaulvtl@gmail.com>
2024-08-02 23:37:01 +0530

    Added class library creation project calculatorApp

---
+++ Assignments/CalculatorApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using CalculatorLibrary;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CalculatorLibrary.Calculator calculator = new CalculatorLibrary.Calculator(); 
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Sum(3, 7));        }
+    }
+}
