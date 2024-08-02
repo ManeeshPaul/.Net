f446af770ebed81c9123976103caad422716efab
maneesh <maneeshpaulvtl@gmail.com>
2024-08-02 23:49:12 +0530

    Multiple interface problem added

---
+++ Assignments/MultipleInterfaceIssue/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MultipleInterfaceIssue
+{
+    interface IWholeSeller
+    {
+        void Discount();
+    }
+    interface IRetailer
+    {
+        void Discount();
+    }
+    class Buyer : IWholeSeller, IRetailer
+    {
+        void IWholeSeller.Discount()
+        {
+            Console.WriteLine("I am called from Demo IWholeSeller");
+        }
+        void IRetailer.Discount()
+        {
+            Console.WriteLine("I am called from Demo IRetailer");
+        }
+        void Print()
+        {
+
+        }
+
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IWholeSeller d1 = new Buyer();
+            d1.Discount();
+
+            IRetailer d2 = new Buyer();
+            d2.Discount();
+
+        }
+    }
+}
