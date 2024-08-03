4a92f18edb66d62b1e81502083fef4a0022ee5ce
maneesh <maneeshpaulvtl@gmail.com>
2024-08-03 13:39:50 +0530

    File class project added

---
+++ Assignments/FileClassProject/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FileClassProject
+{
+    internal class FileDemoClass
+    {
+        public static void Main(string[] arg)
+        {
+
+            string sourceFilePath = @"C:\Users\Administrator\Desktop\CSharp\File_Handling\Sample.txt";
+            string destinationFilePath = @"C:\Users\Administrator\Desktop\CSharp\File_Handling\DestinationFile.txt";
+
+            if (File.Exists(destinationFilePath))
+            {
+                File.Copy(sourceFilePath, destinationFilePath, true);
+                //File.Delete(destinationFilePath);
+                //File.Create(destinationFilePath);   
+            }
+            else
+            {
+                Console.WriteLine("Destination file does not exists");
+            }
+        }
+    }
+}
