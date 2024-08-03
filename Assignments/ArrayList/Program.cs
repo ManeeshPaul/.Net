32d07e3bd526a14184cf03d5f8155a5bd2cebd60
maneesh <maneeshpaulvtl@gmail.com>
2024-08-03 13:34:12 +0530

    array list project added

---
+++ Assignments/ArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace ArrayListProject
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            ArrayList aList = new ArrayList();
+            string str = "Arun";
+            int a = 10;
+            DateTime dt = DateTime.Parse("30-08-1998");
+
+            aList.Add(str);
+            aList.Add(a);
+            aList.Add(dt);
+
+            foreach (var val in aList)
+            {
+                Console.WriteLine(val);
+            }
+
+
+            Console.WriteLine("**************************");
+
+            Hashtable ht = new Hashtable();
+            ht.Add("cs", "cs.net");
+            ht.Add("vb", "vb.net");
+            ht.Add("asp", "asp.net");
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+    }
+}
