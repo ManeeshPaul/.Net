24ba3c0d8f481c8eddb0a1303b9c4a62f86838cc
maneesh <maneeshpaulvtl@gmail.com>
2024-08-10 00:10:50 +0530

    Added LINQ program

---
+++ Assignments/LINQProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LINQProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] languages = { "java", "php", "python", "c++", "c", "c#" };
+            var result = from lang in languages where lang.Contains('c') select lang;
+
+            foreach (var lan in result)
+            {
+                Console.WriteLine(lan);
+            }
+        }
+    }
+}
