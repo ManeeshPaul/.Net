02ff5d7e02544483372fec371581254cc237fcee
maneesh <maneeshpaulvtl@gmail.com>
2024-08-11 15:03:45 +0530

    XSLTDemo program added

---
+++ Assignments/XSLTDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Xsl;
+
+namespace XSLTDemo
+{
+    internal class XSLTDemo
+    {
+        public static void Main()
+        {
+            XslTransform xslt = new XslTransform();
+            xslt.Load("C:\\Users\\Administrator\\Desktop\\CSharp\\Tasks\\Assignments\\XSLT\\Sample.xsl");
+            xslt.Transform("C:\\Users\\Administrator\\Desktop\\CSharp\\Tasks\\Assignments\\XMLFiles\\Books.xml", "file.html");
+
+
+            //Here file.html path will be "C:\Users\Administrator\Desktop\CSharp\FirstProject\Project10_08\bin\Debug\net6.0\file.html"
+        }
+    }
+}
