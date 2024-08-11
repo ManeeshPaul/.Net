eb3c5f15b9a3f55379e2f9dd7f01bf41b05f2879
maneesh <maneeshpaulvtl@gmail.com>
2024-08-11 12:01:05 +0530

    xml write node program added

---
+++ Assignments/XMLWriteNode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml;
+
+namespace XMLWriteNode
+{
+    internal class WriteNode
+    {
+        public static void Main(string[] args)
+        {
+            XmlTextWriter writer = new XmlTextWriter("C:\\Users\\Administrator\\Desktop\\CSharp\\Tasks\\Assignments\\XMLFiles\\BooksDataWriteNode.xml", null);
+            XmlTextReader reader = new XmlTextReader("C:\\Users\\Administrator\\Desktop\\CSharp\\Tasks\\Assignments\\XMLFiles\\Books.xml");
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    writer.WriteNode(reader, true);
+                }
+            }
+            writer.Close();
+        }
+    }
+}
