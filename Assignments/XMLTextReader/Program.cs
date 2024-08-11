1378ed63bbffdf645e7830908e117dcea61c7f83
maneesh <maneeshpaulvtl@gmail.com>
2024-08-11 11:52:58 +0530

    XML Textreader and text writer programs added

---
+++ Assignments/XMLTextReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+
+namespace XMLTextReader
+{
+    internal class TextReader
+    {
+        public static void Main(string[] args)
+        {
+            XmlTextReader reader = new XmlTextReader("C:\\Users\\Administrator\\Desktop\\CSharp\\Tasks\\Assignments\\XMLFiles\\Books.xml");
+            Console.WriteLine(reader.Name);
+            Console.WriteLine(reader.BaseURI);
+            Console.WriteLine(reader.LocalName);
+            while (reader.Read())
+            {
+                if (reader.HasValue)
+                {
+                    //Console.WriteLine("Name: "+reader.Value);
+                    //Console.WriteLine("Node Depth: " + reader.Depth.ToString());
+                    Console.WriteLine("Value: " + reader.Value);
+
+                }
+            }
+        }
+    }
+}
