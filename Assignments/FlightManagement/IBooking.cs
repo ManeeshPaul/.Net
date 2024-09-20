da81298a6e862692e081a9184a30106ee74f488a
maneesh <maneeshpaulvtl@gmail.com>
2024-09-20 19:48:16 +0530

    Milestone Assessment 3 FlightManagement Program added

---
+++ Assignments/FlightManagement/IBooking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagement
+{
+    public interface IBooking
+    {
+        void BookTicket(Flight flight, Passenger passenger);
+        int GetBookingId(); // Method to get booking ID
+        void CancelBooking(int bookingId);
+        string GetBookingDetails(int bookingId);
+    }
+
+
+}
